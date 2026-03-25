854d5facd59b3a8921235cf186e63b575dce7dfe
Lucas Daniel <danielfamouz123@gmail.com>
2026-03-25 00:10:40 -0400

    Alteração na venda de peças.

--- vendasdepecas.py
+++ vendasdepecas.py
@@ -1,12 +1,27 @@
-c = 200
+c = 200  # Variável para especificar o valor do lucro.
 
-mes = input("Digite o mês que deseja receber o resultado: ")
+mes = int(input("Digite o mês que deseja receber o resultado: "))
 
-mes = int(mes)
-
-r = c * mes
+r = c * mes # Variável para multiplicar o mês digitado pelo usuário(a).
 
 print(f"A quantidade peças para o mês {mes} será {r}.")
+
+
+# Calculadora de IR(Imposto de Renda) dos funcionários da empresa.
+
+salario = int(input('Digite seu salário:'))
+
+if salario <= 1621.00:
+    print("Isento de IR.")
+elif salario <= 2826:
+    print("Pagará 7.5% de IR.")  
+elif salario <= 3751:
+    print("Pagará 15% de IR.")     
+elif salario <= 4664:
+    print("Pagará 22.5% de IR.")    
+else:
+    print("Pagará 27.5% de IR.")      
+
 
 
 
