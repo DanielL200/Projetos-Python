15843cd353685ba0295ca0a62290efb32894afab
Lucas Daniel <danielfamouz123@gmail.com>
2026-04-30 17:39:38 -0400

    Update da Loja.

--- Loja_de_Utensílios.py
+++ Loja_de_Utensílios.py
@@ -1,3 +1,5 @@
+# Calculadora de lucro mensal da empresa:
+
 c = 200  # Variável para especificar o valor do lucro.
 
 mes = int(input("Digite o mês que deseja receber o resultado: "))
@@ -20,7 +22,28 @@ elif salario <= 3751:
 elif salario <= 4664:
     print("Pagará 22.5% de IR.")    
 else:
-    print("Pagará 27.5% de IR.")      
+    print("Pagará 27.5% de IR.")   
+
+
+# Código para calcular os descontos da loja:    
+
+
+valor = float(input('Digite o valor da compra: R$ '))
+
+if valor >= 1000:
+    desconto = valor * 0.15
+elif valor >= 500:
+    desconto = valor * 0.10
+elif valor >= 100:
+    desconto = valor * 0.05
+else:
+    desconto = 0
+
+valor_final = valor - desconto
+
+print(f"Valor original: R$ {valor:.2f}")      
+print(f"Desconto: R$ {desconto:.2f}")        
+print(f"Valor final: R$ {valor_final:.2f}")         
 
 
 
