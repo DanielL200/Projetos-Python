22c429a2a83a1816cec54950baac8a04aec47346
Lucas Daniel <danielfamouz123@gmail.com>
2026-04-30 17:59:51 -0400

    New project.

--- Loja_de_Utensílios.py
+++ Loja_de_Utensílios.py
@@ -1,49 +1,49 @@
-# Calculadora de lucro mensal da empresa:
+# Função para calcular o lucro obtido no mês:
 
-c = 200  # Variável para especificar o valor do lucro.
-
-mes = int(input("Digite o mês que deseja receber o resultado: "))
-
-r = c * mes # Variável para multiplicar o mês digitado pelo usuário(a).
-
-print(f"A quantidade peças para o mês {mes} será {r}.")
+def calcular_lucro():
+    lucro_por_mes = 200
+    mes = int(input("Digite o mês: "))
+    lucro_total = lucro_por_mes * mes
+    print(f"Lucro no mês {mes}: {lucro_total}")
 
 
 # Calculadora de IR(Imposto de Renda) dos funcionários da empresa.
 
-salario = int(input('Digite seu salário:'))
+def calcular_ir():
+    salario = float(input("Digite seu salário: R$"))    
 
-if salario <= 1621.00:
-    print("Isento de IR.")
-elif salario <= 2826:
-    print("Pagará 7.5% de IR.")  
-elif salario <= 3751:
-    print("Pagará 15% de IR.")     
-elif salario <= 4664:
-    print("Pagará 22.5% de IR.")    
-else:
-    print("Pagará 27.5% de IR.")   
-
-
-# Código para calcular os descontos da loja:    
+    if salario <= 1621.00:
+        print("Isento de IR(Imposto de Renda).")
+    elif salario <= 2826:
+        print("Pagará 7.5% de IR(Imposto de Renda).")  
+    elif salario <= 3751:
+        print("Pagará 15% de IR(Imposto de Renda).")     
+    elif salario <= 4664:
+        print("Pagará 22.5% de IR(Imposto de Renda).")    
+    else:
+        print("Pagará 27.5% de IR(Imposto de Renda).")   
 
 
-valor = float(input('Digite o valor da compra: R$ '))
+# Função para calcular o desconto do preço do produto:
 
-if valor >= 1000:
-    desconto = valor * 0.15
-elif valor >= 500:
-    desconto = valor * 0.10
-elif valor >= 100:
-    desconto = valor * 0.05
-else:
-    desconto = 0
+def calcular_desconto():
+    valor = float(input('Digite o valor da compra: R$ '))
 
-valor_final = valor - desconto
+    if valor >= 1000:
+        desconto = valor * 0.15
+    elif valor >= 500:
+        desconto = valor * 0.10
+    elif valor >= 100:
+        desconto = valor * 0.05
+    else:
+        desconto = 0
 
-print(f"Valor original: R$ {valor:.2f}")      
-print(f"Desconto: R$ {desconto:.2f}")        
-print(f"Valor final: R$ {valor_final:.2f}")         
+    print(f"Valor final: R$ {valor - desconto:.2f}")
+
+
+calcular_lucro()
+calcular_ir()
+calcular_desconto()
 
 
 
